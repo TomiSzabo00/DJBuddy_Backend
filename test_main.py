354bc66be2a0b4a950694dff332bb914fe496e5e
Szabo Tamas <tamas.szabo0102@gmail.com>
2024-04-23 19:26:01 +0200

    Update api address

--- test_main.py
+++ test_main.py
@@ -13,7 +13,7 @@ def test_login_correct():
         "password": "dj"
     }
     response = client.post(
-        "/users/login",
+        "api/users/login",
         json=jsonable_encoder(correct_details)
     )
     assert response.status_code == 200
@@ -24,7 +24,7 @@ def test_login_incorrect():
         "password": ""
     }
     response = client.post(
-        "/users/login",
+        "api/users/login",
         json=jsonable_encoder(incorrect_details)
     )
     assert response.status_code == 401
@@ -32,53 +32,53 @@ def test_login_incorrect():
 def test_get_events():
     user_id = "7e631e89-bfab-49c7-8139-3cc451b5cdf7"
     response = client.get(
-        "/users/%s/events" % user_id,
+        "api/users/%s/events" % user_id,
     )
     assert response.status_code == 200
 
 def test_get_events_no_user():
     user_id = "0"
     response = client.get(
-        "/users/%s/events" % user_id,
+        "api/users/%s/events" % user_id,
     )
     assert response.status_code == 404
 
 @pytest.mark.anyio
 async def test_update_user_balance():
     user_id = "7e631e89-bfab-49c7-8139-3cc451b5cdf7"
-    async with AsyncClient(app=app, base_url="http://127.0.0.1") as ac:
-        user = await ac.get("/users/%s" % user_id)
+    async with AsyncClient(app=app, base_url="https://djbuddy.online") as ac:
+        user = await ac.get("api/users/%s" % user_id)
         current_balance = float(user.json()["balance"])
         assert current_balance is not None, "User has no balance"
 
         amount = 100
-        new_amount = await ac.put("/users/%s/balance/%s" % (user_id, amount))
+        new_amount = await ac.put("api/users/%s/balance/%s" % (user_id, amount))
         assert float(new_amount.json()) == current_balance + amount, "User balance not added correctly"
 
-        old_amount = await ac.put("/users/%s/balance/%s/remove" % (user_id, amount))
+        old_amount = await ac.put("api/users/%s/balance/%s/remove" % (user_id, amount))
         assert float(old_amount.json()) == current_balance, "User balance not subtracted correctly"
 
 @pytest.mark.anyio
 async def test_update_user_balance_negative():
     user_id = "7e631e89-bfab-49c7-8139-3cc451b5cdf7"
-    async with AsyncClient(app=app, base_url="http://127.0.0.1") as ac:
-        user = await ac.get("/users/%s" % user_id)
+    async with AsyncClient(app=app, base_url="https://djbuddy.online") as ac:
+        user = await ac.get("api/users/%s" % user_id)
         current_balance = float(user.json()["balance"])
         assert current_balance is not None, "User has no balance"
 
-        remove = await ac.put("/users/%s/balance/%s/remove" % (user_id, current_balance + 1))
+        remove = await ac.put("api/users/%s/balance/%s/remove" % (user_id, current_balance + 1))
         assert remove.status_code == 400, "User balance could be subtracted to negative"
 
 def test_get_event_theme():
     theme = "61b09489-6525-47a5-b473-3bc22dc6756a"
     response = client.get(
-        "/events/%s/theme" % theme,
+        "api/events/%s/theme" % theme,
     )
     assert response.status_code == 200, "Event theme not found"
 
 def test_get_event_theme_not_found():
     theme = "notfound"
     response = client.get(
-        "/events/%s/theme" % theme,
+        "api/events/%s/theme" % theme,
     )
     assert response.status_code == 400, "Event theme found"
